cdfb7af189f5dcbd7b3307842e6e588fcc72bb09
Daiji Fukagawa <dfukagaw28@gmail.com>
2020-03-18 07:45:49 +0900

    Fix update_mia.sh

--- update_mia.sh
+++ update_mia.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-export FILELIST='\
-    lib/js/annot/annotorious.css \
-    lib/js/annot/annotorious.min.js \
-    lib/js/my/annotext.js \
-    lib/js/my/avannot.js \
-    lib/js/my/imgannot.css \
-    lib/js/my/imgannot.js \
-    lib/js/mt/webannotorious.js \
-    parts/kan01.css \
-    parts/loading-c.gif \
-    parts/loading.gif \
-    parts/netref.gif \
-    parts/simpleg.woff \
-    parts/std.js \
-    parts/tri-r.png \
-    parts/www.png \
+export FILELIST='
+    lib/js/annot/annotorious.css
+    lib/js/annot/annotorious.min.js
+    lib/js/my/annotext.js
+    lib/js/my/avannot.js
+    lib/js/my/imgannot.css
+    lib/js/my/imgannot.js
+    lib/js/mt/webannotorious.js
+    parts/kan01.css
+    parts/loading-c.gif
+    parts/loading.gif
+    parts/netref.gif
+    parts/simpleg.woff
+    parts/std.js
+    parts/tri-r.png
+    parts/www.png
     works/2016/annot/mushierami.json'
 
 for f in $FILELIST
